85059c1d96b136b06f86bc78f3615d7a7195fa11
putuprema <iputupremaananda@gmail.com>
2019-06-22 10:22:54 +0700

    implement custom student names

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import RowsAndColumnsSelector from './RowsAndColumnsSelector'
 import ChairTable from './ChairTable'
 import Buttons from './Buttons'
+import NamesForm from './NamesForm'
 
 class MainComponent extends React.Component {
   constructor() {
@@ -10,21 +11,23 @@ class MainComponent extends React.Component {
       columns: 4,
       rows: 5,
       tableData: [],
-      names: ["Putu", "Budi", "Dora", "Ahmad", "Santoso"]
+      names: ["Putu", "Budi", "Dora", "Ahmad", "Santoso"],
+      isNamesFormShown: false
     }
     this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
+    this.updateNames = this.updateNames.bind(this)
   }
 
   componentDidMount() {
-    this.generateTableData("rows", this.state.rows)
+    this.generateTableData("rows", this.state.rows, this.state.names)
   }
 
-  generateTableData(name, value) {
+  generateTableData(rows_or_columns, value, studentNames) {
     let nameIdxAssigned = [];
     let n_namesAssigned = 0;
     let arr = [];
-    if (name === "rows") {
+    if (rows_or_columns === "rows") {
       for (let i = 0; i < value; i++) {
         arr[i] = {id: i, columns: []};
 
@@ -32,11 +35,11 @@ class MainComponent extends React.Component {
           let idx;
 
           do {
-            idx = Math.floor(Math.random() * this.state.names.length);
-          } while (n_namesAssigned < this.state.names.length && nameIdxAssigned[idx] === "true");
+            idx = Math.floor(Math.random() * studentNames.length);
+          } while (n_namesAssigned < studentNames.length && nameIdxAssigned[idx] === "true");
 
-          if (n_namesAssigned < this.state.names.length) {
-            arr[i].columns[j] = this.state.names[idx];
+          if (n_namesAssigned < studentNames.length) {
+            arr[i].columns[j] = studentNames[idx];
             nameIdxAssigned[idx] = "true";
             n_namesAssigned++;
           }
@@ -44,7 +47,7 @@ class MainComponent extends React.Component {
         }
       }
     }
-    else if (name === "columns") {
+    else if (rows_or_columns === "columns") {
       for (let i = 0; i < this.state.rows; i++) {
         arr[i] = {id: i, columns: []};
         
@@ -52,11 +55,11 @@ class MainComponent extends React.Component {
           let idx;
 
           do {
-            idx = Math.floor(Math.random() * this.state.names.length);
-          } while (n_namesAssigned < this.state.names.length && nameIdxAssigned[idx] === "true");
+            idx = Math.floor(Math.random() * studentNames.length);
+          } while (n_namesAssigned < studentNames.length && nameIdxAssigned[idx] === "true");
 
-          if (n_namesAssigned < this.state.names.length) {
-            arr[i].columns[j] = this.state.names[idx];
+          if (n_namesAssigned < studentNames.length) {
+            arr[i].columns[j] = studentNames[idx];
             nameIdxAssigned[idx] = "true";
             n_namesAssigned++;
           }
@@ -66,35 +69,47 @@ class MainComponent extends React.Component {
     }
 
     this.setState(
-      {tableData: arr}
+      {tableData: arr, names: studentNames}
     )
   }
 
   handleChange(event) {
     const {name, value} = event.target
     if (name === "columns" || name === "rows") {
-      this.generateTableData(name, value)
+      this.generateTableData(name, value, this.state.names)
+      this.setState (
+        {[name]: parseInt(value, 10)}
+      )
     }
-    this.setState (
-      {[name]: parseInt(value, 10)}
-    )
+    else {
+      this.setState (
+        {[name]: value}
+      )
+    }
   }
 
   handleClick(event) {
     const {name} = event.target
     switch (name) {
-      case "gen": this.generateTableData("rows", this.state.rows); break;
+      case "gen": this.generateTableData("rows", this.state.rows, this.state.names); break;
+      case "updateName": this.setState(prevState => ( {isNamesFormShown: !prevState.isNamesFormShown} )); break;
+      case "reset": window.location.reload(); break;
       default: break;
     }
   }
+
+  updateNames(studentNames) {
+    this.generateTableData("rows", this.state.rows, studentNames.split(", "));
+  }
   
   render() {
-    console.log(this.state)
+    // console.log(this.state)
     return (
       <div className="MainComponent">
         <RowsAndColumnsSelector handleChange={this.handleChange} state={this.state} /> <br />
         <ChairTable state={this.state} /> <br />
-        <Buttons handleClick={this.handleClick} />
+        <Buttons handleClick={this.handleClick} /> <br />
+        <NamesForm updateNames={this.updateNames} enabled={this.state.isNamesFormShown} /> <br />
       </div>
     )
   }
---
+++ src/components/NamesForm.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+// function NamesForm(props) {
+//   return (
+//     <div className="names-form-container">
+//       <h2>Student Names</h2>
+//       <p>Here you can type your student names. Each student will be assigned a seat randomly.</p>
+//       <input name="namesInputForm" type="text" placeholder="Input your student names here..." value={props.state.namesInputForm} onChange={props.handleChange}></input>
+//       <button name="namesSubmit" onClick={props.handleClick}>Update</button>
+//     </div>
+//   )
+// }
+
+class NamesForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      names: ""
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('Student names were submitted: ' + this.state.names);
+    this.props.updateNames(this.state.names);
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({names: event.target.value});
+  }
+
+  render() {
+    if (this.props.enabled) {
+      return (
+        <div className="names-form-container">
+          <h2>Student Names</h2>
+          <p>Here you can type your student names. Each student will be assigned a seat randomly.</p>
+          <form onSubmit={this.handleSubmit}>
+            <input id="studentNames" type="text" value={this.state.names} onChange={this.handleChange} placeholder="Input student names here (e.g. Jojo, Lili, Bibi)"/>
+            <input type="submit" value="Update" />
+        </form>
+        </div>
+      )
+    }
+    return <div></div>;
+  }
+}
+
+export default NamesForm
