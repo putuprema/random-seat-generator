6b36fb6435f051a6590256e0743a190b15cd798a
putuprema <iputupremaananda@gmail.com>
2019-06-22 10:24:18 +0700

    add footer and prevent seating table from overflowing from its div container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import MainComponent from './components/MainComponent';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Header />
       <MainComponent />
+      <Footer />
     </div>
   );
 }
