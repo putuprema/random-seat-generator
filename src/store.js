5945b086f0f84ead2555b3485ebd48915b29ae71
putuprema <iputupremaananda@gmail.com>
2019-06-25 17:29:07 +0700

    use normal compose when redux devtools is not installed on client

--- src/store.js
+++ src/store.js
@@ -6,13 +6,12 @@ const initialState = {};
 
 const middleWare = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(...middleWare))
 );
 
 export default store;
